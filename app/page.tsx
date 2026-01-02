9b4745c9f796d1c8f690adbecf6c004486d7077c
kirikiritanpo <121788724+kirikiritanpo@users.noreply.github.com>
2026-01-02 13:22:37 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { CatImage } from "./cat-image";
-import { fetchImage } from "./fetch-image";
-
-export default async function Home() {
-  const image = await fetchImage();
-  return <CatImage url={image.url} />;
-}
